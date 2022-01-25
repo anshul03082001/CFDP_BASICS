b8bcdfd123c1b3d137f7fb699ee9125e375b3e55
ANSHUL THAKUR <91362007+anshul03082001@users.noreply.github.com>
2022-01-25 11:02:31 +0530

    Add files via upload

---
+++ cmake_program/build/CMakeFiles/cmake_program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_program.dir/main.cpp.o"
+  "cmake_program"
+  "cmake_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
